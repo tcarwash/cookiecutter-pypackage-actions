250938ff7c4da319a010bf245f33137c021e7824
Tyler Carr <t.carr2@students.clark.edu>
2022-09-15 10:55:11 -0700

    Update __init__.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+from . import cli, {{ cookiecutter.project_slug }}
